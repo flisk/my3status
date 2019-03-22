ee5569ccfcc0e28e4df5c71b5f7aa3753b2580f4
Flisk <github@flisk.xyz>
2019-03-22 08:12:56 +0100

    Add battery level display

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 CFLAGS ?= -Wall -Werror=format-security -Werror=implicit-function-declaration \
-		  -Wextra -O2
+		  -Wextra -O2 \
+		  `pkg-config --cflags --libs gio-2.0`
+
 my3status: my3status.c
--- my3status.c
+++ my3status.c
@@ -1,11 +1,14 @@
 /* vim: set noet ts=8 sw=8: */
 #include <sys/sysinfo.h>
+#include <sys/vfs.h>
 #include <errno.h>
 #include <error.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include <gio/gio.h>
 
 #define I3BAR_ITEM(name, full_text) \
 	printf("{\"name\":\"" name "\",\"full_text\":\""); \
@@ -21,21 +24,126 @@ void print_datetime(struct tm *tm, char *timebuf) {
 	printf("%s", timebuf);
 }
 
-void print_sysinfo(struct sysinfo *si) {
+/*
+ * Battery charge level and remaining time
+ */
+static GVariant *get_upower_property(
+		GDBusConnection *conn,
+		const char *prop_name) {
+	GError *err = NULL;
+	GVariant *res = g_dbus_connection_call_sync(
+			conn,
+			"org.freedesktop.UPower",
+			"/org/freedesktop/UPower/devices/DisplayDevice",
+			"org.freedesktop.DBus.Properties",
+			"Get",
+			g_variant_new("(ss)",
+				"org.freedesktop.UPower.Device",
+				prop_name),
+			NULL,
+			G_DBUS_CALL_FLAGS_NONE,
+			-1,
+			NULL,
+			&err);
+
+	if (err) {
+		fprintf(stderr, "g_dbus_connection_call_sync: %i: %s",
+				err->code, err->message);
+		exit(1);
+	}
+
+	GVariant *v;
+	g_variant_get(res, "(v)", &v);
+	g_variant_unref(res);
+
+	return v;
+}
+
+static void format_seconds(gint64 t, char *buf) {
+	gint64 minutes = t % 60;
+	gint64 hours = t / 60 / 60;
+
+	sprintf(buf, "%ld:%02ld", hours, minutes);
+}
+
+static void print_battery(GDBusConnection *conn) {
+	GVariant *p = get_upower_property(conn, "Percentage");
+	GVariant *te = get_upower_property(conn, "TimeToEmpty");
+	GVariant *tf = get_upower_property(conn, "TimeToFull");
+
+	gdouble percent = g_variant_get_double(p);
+	gint64 time_to_empty = g_variant_get_int64(te);
+	gint64 time_to_full = g_variant_get_int64(tf);
+
+	g_variant_unref(p);
+	g_variant_unref(te);
+	g_variant_unref(tf);
+
+	if (time_to_empty > 0) { // Discharging
+		char buf[32];
+		format_seconds(time_to_empty, buf);
+
+		I3BAR_ITEM("battery", printf("🔋 %d%% (%s)",
+					(int) percent, buf));
+	} else if (time_to_full > 0) { // Charging
+		char buf[32];
+		format_seconds(time_to_full, buf);
+
+		I3BAR_ITEM("battery", printf("🔋⚡ %d%% (%s)",
+					(int) percent, buf));
+	}
+}
+
+#define UNICODE_LINUX_BIRD "\xf0\x9f\x90\xa7"
+
+/*
+ * System load (5 minute average) and concise uptime
+ */
+static void print_sysinfo(struct sysinfo *si) {
 	float load_5min = si->loads[0] / (float) (1 << SI_LOAD_SHIFT);
-	long up_hours = si->uptime / 3600;
-	long up_days = up_hours / 24;
+	long up_hours   = si->uptime / 3600;
+	long up_days    = up_hours / 24;
 
 	up_hours -= up_days * 24;
 
-	printf("%.2f up %ldd %ldh", load_5min, up_days, up_hours);
+	printf(UNICODE_LINUX_BIRD " %.2f %ldd %ldh",
+			load_5min, up_days, up_hours);
+}
+
+#define UNICODE_FLOPPY "\xf0\x9f\x92\xbe"
+
+/*
+ * Percentage of used space on the filesystem
+ */
+static void print_fs_usage(struct statfs *s) {
+	unsigned long total = s->f_blocks;
+	unsigned long used  = total - s->f_bfree;
+	float used_percent  = (100.0f / total) * used;
+
+	printf(UNICODE_FLOPPY " %.1f%%", used_percent);
+}
+
+static GDBusConnection *dbus_system_connect() {
+	GDBusConnection *conn;
+	GError *err = NULL;
+
+	conn = g_bus_get_sync(G_BUS_TYPE_SYSTEM, NULL, &err);
+	if (err) {
+		fprintf(stderr, "g_bus_get_sync: %i: %s\n",
+				err->code, err->message);
+		exit(1);
+	}
+
+	return conn;
 }
 
 int main() {
-	struct sysinfo si;
-	struct tm *tm;
-	time_t tt;
-	char timebuf[TIMEBUF_SIZE];
+	struct tm	*tm;
+	time_t		 tt;
+	char		 timebuf[TIMEBUF_SIZE];
+	struct sysinfo	 si;
+	struct statfs	 sfs;
+	GDBusConnection	*conn;
 
 	// This stops glibc from doing a superfluous stat() for every
 	// strftime(). It's an asinine micro-optimization, but, for fun and no
@@ -43,23 +151,28 @@ int main() {
 	if (setenv("TZ", ":/etc/localtime", 0) == -1)
 		error(1, errno, "setenv");
 
+	conn = dbus_system_connect();
+
 	puts("{\"version\": 1}\n[\n[]");
 	while (1) {
-		if (sysinfo(&si) != 0)
-			error(1, errno, "sysinfo");
-
 		tt = time(NULL);
 		if ((tm = localtime(&tt)) == NULL)
 			error(1, errno, "localtime");
 
+		if (sysinfo(&si) != 0)
+			error(1, errno, "sysinfo");
+
+		if (statfs("/", &sfs) != 0)
+			error(1, errno, "statfs");
 
 		printf(",[");
-
-		I3BAR_ITEM("uptime", print_sysinfo(&si));
+		I3BAR_ITEM("fsusage", print_fs_usage(&sfs));
+		I3BAR_ITEM("sysinfo", print_sysinfo(&si));
+		print_battery(conn);
 		I3BAR_ITEM("datetime", print_datetime(tm, timebuf));
+		printf("]\n");
 
-		printf("]");
 		fflush(stdout);
-		sleep(5);
+		sleep(10);
 	}
 }
